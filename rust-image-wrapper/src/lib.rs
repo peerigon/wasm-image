7b1b535aacb89ba68f5c742b529bbe0aefbf0a7f
Simon Abbt <simon.abbt@peerigon.com>
2019-03-04 17:54:31 +0100

    extracted parse functions, cleanup and refactoring

--- rust-image-wrapper/src/lib.rs
+++ rust-image-wrapper/src/lib.rs
@@ -1,14 +1,18 @@
 extern crate cfg_if;
-extern crate wasm_bindgen;
-extern crate thread_local;
 extern crate image;
+extern crate thread_local;
+extern crate wasm_bindgen;
+
+use std::io::{Cursor, Read, Seek, SeekFrom};
 
 mod utils;
 
-use std::str;
 use cfg_if::cfg_if;
-use wasm_bindgen::prelude::*;
+use image::DynamicImage;
+use image::GenericImageView;
 use image::ImageFormat;
+use std::str;
+use wasm_bindgen::prelude::*;
 
 #[macro_use]
 pub mod log;
@@ -24,28 +28,50 @@ cfg_if! {
 }
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn rotate(_buffer: &[u8], _deg: u16) -> Vec<u8> {
-    /*let buf_str = match str::from_utf8(_buffer) {
-        Ok(v) => v,
-        Err(e) => {
-            log!("Invalid UTF-8 sequence: {}", e);
-            panic!("Invalid UTF-8 sequence: {}", e);
-        },
-    };*/
-    log!("doing the image thing11");
-
-    let mut img = match image::load_from_memory_with_format(_buffer, ImageFormat::PNG) {
+fn load_image_from_array(_array: &[u8]) -> DynamicImage {
+    let img = match image::load_from_memory_with_format(_array, ImageFormat::PNG) {
         Ok(img) => img,
         Err(error) => {
             log!("There was a problem opening the file: {:?}", error);
             panic!("There was a problem opening the file: {:?}", error)
-        },
+        }
     };
+    return img;
+}
+
+fn get_image_as_array(_img: DynamicImage) -> Vec<u8> {
+    // Create fake "file"
+    let mut c = Cursor::new(Vec::new());
+
+    match _img.write_to(&mut c, ImageFormat::PNG) {
+        Ok(c) => c,
+        Err(error) => {
+            log!(
+                "There was a problem writing the resulting buffer: {:?}",
+                error
+            );
+            panic!(
+                "There was a problem writing the resulting buffer: {:?}",
+                error
+            )
+        }
+    };
+    c.seek(SeekFrom::Start(0)).unwrap();
+
+    // Read the "file's" contents into a vector
+    let mut out = Vec::new();
+    c.read_to_end(&mut out).unwrap();
+
+    return out;
+}
+
+#[wasm_bindgen]
+pub fn rotate(_array: &[u8], _deg: u16) -> Vec<u8> {
+    let mut img = load_image_from_array(_array);
 
     img = match _deg {
         90 => img.rotate90(),
@@ -54,18 +80,19 @@ pub fn rotate(_buffer: &[u8], _deg: u16) -> Vec<u8> {
         _ => img,
     };
 
-    return img.to_rgb().into_raw();
+    return get_image_as_array(img);
 }
 
 #[wasm_bindgen]
-pub fn resize(_buffer: &[u8], _width: u32, _height: u32, _filter: u8, _aspect_ratio_preserve: bool) -> Vec<u8> {
-    /*let buf_str = match str::from_utf8(_buffer) {
-        Ok(v) => v,
-        Err(e) => {
-            log!("Invalid UTF-8 sequence: {}", e);
-            panic!("Invalid UTF-8 sequence: {}", e);
-        },
-    };*/
+pub fn resize(
+    _array: &[u8],
+    _width: u32,
+    _height: u32,
+    _filter: u8,
+    _aspect_ratio_preserve: bool,
+) -> Vec<u8> {
+    log!("Received buffer");
+
     let _checked_filter: image::FilterType = match _filter {
         0 => image::FilterType::Nearest,
         1 => image::FilterType::Lanczos3,
@@ -74,21 +101,22 @@ pub fn resize(_buffer: &[u8], _width: u32, _height: u32, _filter: u8, _aspect_ra
         4 => image::FilterType::Triangle,
         _ => image::FilterType::Nearest,
     };
-    log!("doing the image thing11");
 
-    let mut img = match image::load_from_memory_with_format(_buffer, ImageFormat::PNG) {
+    let mut img: DynamicImage = match image::load_from_memory_with_format(_array, ImageFormat::PNG)
+    {
         Ok(img) => img,
         Err(error) => {
             log!("There was a problem opening the file: {:?}", error);
             panic!("There was a problem opening the file: {:?}", error)
-        },
+        }
     };
 
     if _aspect_ratio_preserve {
         img = img.resize(_width, _height, _checked_filter);
     } else {
         img = img.resize_exact(_width, _height, _checked_filter);
-    }
+    };
 
-    return img.to_rgb().into_raw();
-}
+    log!("Sends array back");
+    return get_image_as_array(img);
+;}
