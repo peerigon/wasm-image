5fc2ff54c73e9abc5255f105a2fb13a4bf17368a
Carsten <git@datahugger.de>
2019-04-09 09:26:18 +0200

    chore(core): add pkg folder

---
+++ rust-image-wrapper/pkg/wasm_rust_image.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+export function rotate(arg0: Uint8Array, arg1: number): Uint8Array;
+export function grayscale(arg0: Uint8Array): Uint8Array;
+export function invert(arg0: Uint8Array): Uint8Array;
+export function blur(arg0: Uint8Array, arg1: number): Uint8Array;
+export function unsharpen(arg0: Uint8Array, arg1: number, arg2: number): Uint8Array;
+export function adjust_contrast(arg0: Uint8Array, arg1: number): Uint8Array;
+export function brighten(arg0: Uint8Array, arg1: number): Uint8Array;
+export function hue_rotate(arg0: Uint8Array, arg1: number): Uint8Array;
+export function flip(arg0: Uint8Array, arg1: number): Uint8Array;
+export function crop(arg0: Uint8Array, arg1: number, arg2: number, arg3: number, arg4: number): Uint8Array;
+export function resize(arg0: Uint8Array, arg1: number, arg2: number, arg3: number, arg4: boolean, arg5: boolean, arg6: boolean): Uint8Array;
