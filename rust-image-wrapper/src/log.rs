090d44bea9192734b4f93a31e0eb830cb57a1513
Johannes Ewald <johannes.ewald@peerigon.com>
2020-12-26 12:17:06 +0100

    Remove files

--- rust-image-wrapper/src/log.rs
+++
@@ -1,9 +0,0 @@
-extern crate web_sys;
-
-// A macro to provide `println!(..)`-style syntax for `console.log` logging.
-#[macro_use]
-macro_rules! log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
